fa2edafe872e7e47517147eeff6a643bcc26a128
VARSITH-PRANAV <varsithpranav71@gmail.com>
2025-07-21 22:23:23 +0530

    Initial commit - QRCode Generator

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {QrCode} from './QrCode.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QrCode/>
+  </StrictMode>,
+)
